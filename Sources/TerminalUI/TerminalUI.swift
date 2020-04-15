4d46207cf7792175a97ae8fb58a1bee8fbc7283b
Leo Dion <leogdion@brightdigit.com>
2020-04-15 09:28:58 -0400

    fixing lint and readme

--- Sources/TerminalUI/TerminalUI.swift
+++ Sources/TerminalUI/TerminalUI.swift
@@ -1,14 +1,13 @@
 struct TerminalUI {
-    var text = "Hello, World!"
-  
+  var text = "Hello, World!"
+
   func execute() {
-    func escapeWith (code : String) {
+    func escapeWith(code: String) {
       print("\u{1B}\(code)", terminator: "")
     }
 
     print("Hello World")
     escapeWith(code: "[1A")
     print("Bye", terminator: "")
-
   }
 }
