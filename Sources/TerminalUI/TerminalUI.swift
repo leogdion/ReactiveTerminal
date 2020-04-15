700d6db5216e8f0875ca7c488f0fbc317ed87457
Leo Dion <leogdion@brightdigit.com>
2020-04-15 10:09:09 -0400

    fixing lint

--- Sources/TerminalUI/TerminalUI.swift
+++ Sources/TerminalUI/TerminalUI.swift
@@ -3,18 +3,15 @@ import Foundation
 public struct TerminalUI {
   var text = "Hello, World!"
 
-  public init () {
-    
-  }
+  public init() {}
 
   func escapeWith(code: String) {
     print("\u{1B}\(code)", terminator: "")
   }
-  
-  public func execute() {
 
+  public func execute() {
     var progress = 0.0
-    while (progress <= 100) {
+    while progress <= 100 {
       print("Progress: \(progress)")
       escapeWith(code: "[1A")
       escapeWith(code: "[2K")
