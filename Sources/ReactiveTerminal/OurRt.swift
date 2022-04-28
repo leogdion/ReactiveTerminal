2e810f88973ca54bb3d8d0d5bcd3e907b0b0c4bb
Leo Dion <leogdion@brightdigit.com>
2022-04-28 10:42:13 -0400

    working text field

--- Sources/ReactiveTerminal/OurRt.swift
+++ Sources/ReactiveTerminal/OurRt.swift
@@ -7,11 +7,13 @@ public struct OurRT : App {
   
   //@Environment(\.refreshRateSeconds) var refreshRate
 
+  @State var text : String = ""
   public var body: some View {
+    
     StackView(spacing: 2){
       Text("Hello World A").foregroundColor(.chartreuse1)
       Text(Int.random(in: 0...1).isMultiple(of: 2) ? "YeS" : "No").color(background: Bool.random() ? Color.chartreuse3A : Color.gold1, foreground: Bool.random() ? Color.darkBlue : Color.sandyBrown)
-      TextField()
+      TextField(text: self.$text)
     }.backgroundColor(.cadetBlueB).padding(.all, 4)
 
   }
--- Sources/ReactiveTerminal/TextField.swift
+++ Sources/ReactiveTerminal/TextField.swift
@@ -22,17 +22,17 @@ final class Box<Value> {
       self.value = value
     }
   
-  static func fetch(_ id: UUID, wrappedValue: Value) -> Self {
-    if let box = AnyBox.boxes[id]?.box as? Self {
-      return box
-    }
-    
-    let box = Self.init(wrappedValue)
-    AnyBox.boxes[id] = .init(box: box)
-    return box
-  }
+//  static func fetch(_ id: UUID, wrappedValue: Value) -> Self {
+//    if let box = AnyBox.boxes[id]?.box as? Self {
+//      return box
+//    }
+//    
+//    let box = Self.init(wrappedValue)
+//    AnyBox.boxes[id] = .init(box: box)
+//    return box
+//  }
 }
-@propertyWrapper struct Binding<Value> {
+@propertyWrapper struct State<Value> {
   
   let box: Box<Value>
  
@@ -46,10 +46,28 @@ final class Box<Value> {
      }
   
   
-  init(id: UUID, wrappedValue: Value) {
-    self.box = Box<Value>.fetch(id, wrappedValue: wrappedValue)
+  init(wrappedValue: Value) {
+    self.box = Box<Value>(wrappedValue)
+  }
+  
+  var projectedValue : Binding<Value> {
+    return .init(box: self.box)
   }
 }
+@propertyWrapper struct Binding<Value> {
+
+  let box: Box<Value>
+  
+   var wrappedValue : Value {
+     get {
+          box.value
+        }
+        nonmutating set {
+          box.value = newValue
+        }
+      }
+}
+
 
 protocol Input {
   var id : UUID { get }
@@ -58,13 +76,17 @@ protocol Input {
 
 struct TextField : View, Input {
   func put(_ value: UInt8) {
-    self.text.append(Character(UnicodeScalar(value)))
+    if value == 127 {
+      self.text.popLast()
+    } else if value >= 32  {
+      self.text.append(Character(UnicodeScalar(value)))
+    }
     
   }
   
-  internal init(cols : Int = 8) {
+  internal init(cols : Int = 8, text: Binding<String>) {
     self.cols = cols
-    self._text = .init(id: self.id, wrappedValue: "")
+    self._text = text
   }
   
   let id: UUID = UUID()
@@ -78,7 +100,12 @@ struct TextField : View, Input {
   }
   
   func render<TerminalViewType>(to view: inout TerminalViewType) where TerminalViewType : TerminalView {
+    view.escapeWith(code: "[4m")
+    
+     view.escapeWith(code: "[7m")
     let text = String((self.text + .init(repeating: "_", count: self.cols)).prefix(self.cols))
     view.write(text)
+    
+    view.escapeWith(code: "[0m")
   }
 }
